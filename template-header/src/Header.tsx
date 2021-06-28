3ea20beca323054497209544093613b5d812bb26
Deberson de Paula <deberson.paula@gmail.com>
2021-06-28 18:45:08 -0300

    added basic template

---
+++ template-header/src/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+function Header() {
+  return <div>HEADER</div>;
+}
+
+export default hot(module)(Header);
---
+++ template-header/webpack.config.js
@@ -0,0 +1,3 @@
+const config = require('@template/config');
+
+module.exports = config({ mode: 'development' });
